cd46c631105c1e2686af1f13b842487e42b79617
Fred Morris <FredrickCPMorris@hotmail.com>
2022-10-10 13:49:32 -0500

    Created user stories & urls file for super_types module + updated vsettings in hearoes and villians app  + updated admin file in super_types + migrated tables over to mysqp.

--- heroes_villians_project/settings.py
+++ heroes_villians_project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'super_types',
-    'super',
+    'supers',
 ]
 
 MIDDLEWARE = [
--- super_types/admin.py
+++ super_types/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import SuperType
 
 # Register your models here.
-admin.sites.register(SuperType)
+admin.site.register(SuperType)
