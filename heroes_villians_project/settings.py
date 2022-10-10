9bcfe6f0a24cb4b9ea26d16a582ab0b8948d2deb
Fred Morris <FredrickCPMorris@hotmail.com>
2022-10-10 12:07:10 -0500

    Updated settings in Heroes and villians file + created serializer file for super module.

--- heroes_villians_project/settings.py
+++ heroes_villians_project/settings.py
@@ -115,6 +115,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 try:
-    from .local_settings import *
+    from heroes_villians_project.local_settings import *
 except ImportError:
     pass
